9d79a7268a56b982942982d60d2b95a53fa21ed0
SRH-Xavier <71642196+SRH-Xavier@users.noreply.github.com>
2021-02-15 21:29:12 +0800

    Draw five edge lines

---
+++ EdgeLines/EdgeLines/EdgeLines.cpp
@@ -0,0 +1,25 @@
+//程序功能：绘制五条边缘直线
+
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+using namespace cv;
+using namespace std;
+int main()
+{
+	Mat srcMat = imread("0.jpg");
+	Mat cannyMat, lineMat;
+	Canny(srcMat, cannyMat, 100, 200, 3);
+	HoughLinesP(cannyMat, lineMat, 1, CV_PI / 180, 50, 50, 10);
+
+	for (int i = 0; i < lineMat.rows; i++)
+	{
+		Vec4i  lines = lineMat.at<Vec4i>(i);
+		line(srcMat, Point(lines[0], lines[1]), Point(lines[2], lines[3]), Scalar(0, 255, 255), 1, CV_AA);
+	}
+	
+	imshow("srcMat", srcMat);
+	waitKey(0);
+	return 0;
+}
+
